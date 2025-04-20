ff701e2a78471edc5eed85372953bd480a9a0bc3
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2025-04-20 12:18:07 +0200

    enable test + viewport

---
+++ lidar-analyzer/src/basic_viewport.rs
@@ -0,0 +1,123 @@
+use eframe::{egui, EventLoopBuilderHook};
+use winit::platform::wayland::EventLoopBuilderExtWayland;
+
+use crate::parse::LidarPoint;
+use crate::units::*;
+
+const DEFAULT_ZOOM: f32 = 180.0;
+
+struct PolarPointsApp {
+    zoom: f32,
+    points: Vec<LidarPoint>, // (angle, distance)
+    offset: egui::Vec2, // Décalage
+    dragging: bool, // Indique si la souris est en train d'être maintenue
+    last_mouse_pos: Option<egui::Pos2>, // Dernière position de la souris
+}
+
+impl Default for PolarPointsApp {
+    fn default() -> Self {
+        let points = (0..360)
+            .map(|i| {
+                let angle = Deg::new(i.into()).rad();
+                let distance = Meters(3.0);
+                LidarPoint {
+                    angle,
+                    distance,
+                    intensity: Intensity::NULL,
+                }
+            })
+            .collect();
+        Self {
+            zoom: DEFAULT_ZOOM,
+            points,
+            offset: egui::vec2(0.0, 0.0),
+            dragging: false,
+            last_mouse_pos: None,
+        }
+    }
+}
+
+impl PolarPointsApp {
+    pub fn new(points: Vec<LidarPoint>) -> Self {
+        Self {
+            zoom: DEFAULT_ZOOM,
+            points,
+            offset: egui::vec2(0.0, 0.0),
+            dragging: false,
+            last_mouse_pos: None,
+        }
+    }
+}
+
+impl eframe::App for PolarPointsApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Vérifier si la touche "H" est pressée
+        if ctx.input(|i| i.key_pressed(egui::Key::H)) {
+            self.zoom = DEFAULT_ZOOM; // Réinitialiser le zoom
+            self.offset = egui::vec2(0.0, 0.0); // Réinitialiser le décalage
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Basic Viewport");
+            ui.label(format!("zoom : {}", self.zoom));
+            ui.label(format!("décallage : {}", self.offset));
+
+            // Zoom avec la molette de la souris
+            let scroll_delta = ui.input(|i| i.smooth_scroll_delta);
+            self.zoom *= 1.0 + scroll_delta.y * 0.01; // Ajustez le facteur de zoom ici
+
+            // Gérer le décalage avec la souris
+            let pointer = ui.input(|i| i.pointer.clone());
+            if pointer.any_released() {
+                self.dragging = false;
+                self.last_mouse_pos = None;
+            }
+
+            if pointer.any_pressed() {
+                self.dragging = true;
+                self.last_mouse_pos = pointer.interact_pos();
+            }
+
+            if self.dragging {
+                if let Some(current_mouse_pos) = pointer.interact_pos() {
+                    if let Some(last_pos) = self.last_mouse_pos {
+                        let delta = current_mouse_pos - last_pos;
+                        self.offset += delta;
+                    }
+                    self.last_mouse_pos = Some(current_mouse_pos);
+                }
+            }
+
+            // Dessiner les points
+            let painter = ui.painter();
+            let center = ui.available_rect_before_wrap().center() + self.offset;
+
+            for point in &self.points {
+                let x = center.x + (point.angle.cos() * point.distance.0) as f32 * self.zoom;
+                let y = center.y + (point.angle.sin() * point.distance.0) as f32 * self.zoom;
+                painter.circle(
+                    egui::pos2(x, y),
+                    5.0,
+                    egui::Color32::GREEN.gamma_multiply(0.5),
+                    egui::Stroke::NONE,
+                );
+            }
+        });
+    }
+}
+
+pub fn show_viewport(points: Vec<LidarPoint>) -> Result<(), eframe::Error> {
+    let event_loop_builder: Option<EventLoopBuilderHook> = Some(Box::new(|event_loop_builder| {
+        event_loop_builder.with_any_thread(true);
+    }));
+    let options = eframe::NativeOptions {
+        event_loop_builder,
+        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 800.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Points en coordonnées polaires",
+        options,
+        Box::new(|_cc| Ok(Box::new(PolarPointsApp::new(points)))),
+    )
+}
--- lidar-analyzer/src/units.rs
+++ lidar-analyzer/src/units.rs
@@ -2,17 +2,46 @@
 //! 
 //! Ce fichier est destiné à être `use units::*` donc peu de choses sont publiques
 
+use std::ops::Div;
+
 pub type Rad = radians::Rad64;
 pub type Deg = radians::Deg64;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Meters(pub f64);
+
+impl Meters {
+    #[inline]
+    pub const fn cm(distance: f64) -> Self {
+        Self(distance / 100.0)
+    }
+
+    #[inline]
+    pub const fn mm(distance: f64) -> Self {
+        Self(distance / 1000.0)
+    }
+
+    pub const fn const_div(self, rhs: Self) -> Self {
+        Self(self.0 / rhs.0)
+    }
+}
+
+impl Div for Meters {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self(self.0 / rhs.0)
+    }
+}
 
 /// Représente une intensité entre 0 et 1 inclus
 #[derive(Debug, Clone, Copy)]
 pub struct Intensity(f64);
 
 impl Intensity {
+    pub const NULL: Intensity = Intensity(0.0);
+    pub const FULL: Intensity = Intensity(1.0);
+
     pub fn new(intensity: f64) -> Result<Intensity, ()> {
         if (0.0..=1.0).contains(&intensity) {
             Ok(Self(intensity))
