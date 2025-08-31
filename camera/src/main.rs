952df335ca139e118304fbcc6303c70a210a41e9
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2025-08-31 21:02:31 +0200

    enable crosscompilation

--- camera/src/main.rs
+++ camera/src/main.rs
@@ -1,12 +1,17 @@
 use std::error::Error;
 
 use libcamera::{
-    camera_manager::CameraManager, framebuffer::AsFrameBuffer, framebuffer_allocator::{FrameBuffer, FrameBufferAllocator}, request::RequestStatus, stream::{Stream, StreamRole}
+    camera_manager::CameraManager,
+    framebuffer::AsFrameBuffer,
+    framebuffer_allocator::{FrameBuffer, FrameBufferAllocator},
+    request::RequestStatus,
+    stream::{Stream, StreamRole},
 };
 
 // documentation of libcamera at : https://libcamera.org/guides/application-developer.html
 
 fn main() -> Result<(), Box<dyn Error>> {
+    panic!("Hello crossing World!");
     let camera_manager = CameraManager::new().expect("cannot initiate camera manager");
 
     let cameras = camera_manager.cameras();
@@ -20,51 +25,51 @@ fn main() -> Result<(), Box<dyn Error>> {
         .generate_configuration(&[StreamRole::VideoRecording])
         .expect("no default conf");
 
-    match default_conf.validate() {
-        CameraConfigurationStatus::Valid => println!("Configuration de la caméra valide !"),
-        CameraConfigurationStatus::Adjusted => {
-            println!("La configuration a été ajustée : {:#?}", default_conf)
-        }
-        CameraConfigurationStatus::Invalid => {
-            panic!("Erreur lors de la validation de la configuration")
-        }
-    }
-    let configuration = default_conf;
-    let stream = configuration
-        .get(0)
-        .expect("malformed config")
-        .stream()
-        .expect("unable to get stream from configuration");
-
-    // Capture images
-
-    // let's use default frame allocator for now
-
-    let allocator = FrameBufferAllocator::new(&camera);
-    // for cfg in configuration.get(0) {
-    //     allocator.alloc(cfg.stream())
+    // match default_conf.validate() {
+    //     CameraConfigurationStatus::Valid => println!("Configuration de la caméra valide !"),
+    //     CameraConfigurationStatus::Adjusted => {
+    //         println!("La configuration a été ajustée : {:#?}", default_conf)
+    //     }
+    //     CameraConfigurationStatus::Invalid => {
+    //         panic!("Erreur lors de la validation de la configuration")
+    //     }
     // }
-    let buffers = allocator.alloc(&stream).expect("cannot allocate buffers");
-    let requests = buffers
-        .iter()
-        .for_each(|buffer| {
-            let mut request = camera
-                .create_request(None)
-                .expect("unable to create request to camera");
-            request
-                .add_buffer(&stream, buffer)
-                .expect("unable to add buffer");
-            request
-        })
-        .collect::<Vec<_>>();
-    
-    camera.on_request_completed(|request| {
-        if request.status() != RequestStatus::Complete {
-            return;
-        }
-        let frame_buffer: &FrameBuffer = request.buffer(&stream).expect("unable to get buffer");
-        println!("metadata {:?}", frame_buffer.metadata());
-    });
+    // let configuration = default_conf;
+    // let stream = configuration
+    //     .get(0)
+    //     .expect("malformed config")
+    //     .stream()
+    //     .expect("unable to get stream from configuration");
+
+    // // Capture images
+
+    // // let's use default frame allocator for now
+
+    // let allocator = FrameBufferAllocator::new(&camera);
+    // // for cfg in configuration.get(0) {
+    // //     allocator.alloc(cfg.stream())
+    // // }
+    // let buffers = allocator.alloc(&stream).expect("cannot allocate buffers");
+    // let requests = buffers
+    //     .iter()
+    //     .for_each(|buffer| {
+    //         let mut request = camera
+    //             .create_request(None)
+    //             .expect("unable to create request to camera");
+    //         request
+    //             .add_buffer(&stream, buffer)
+    //             .expect("unable to add buffer");
+    //         request
+    //     })
+    //     .collect::<Vec<_>>();
+
+    // camera.on_request_completed(|request| {
+    //     if request.status() != RequestStatus::Complete {
+    //         return;
+    //     }
+    //     let frame_buffer: &FrameBuffer = request.buffer(&stream).expect("unable to get buffer");
+    //     println!("metadata {:?}", frame_buffer.metadata());
+    // });
 
     Ok(())
 }
--- shell.nix
+++ shell.nix
@@ -6,9 +6,14 @@ pkgs.mkShellNoCC {
 
   packages = with pkgs; [
     rustup
+    podman # to cross compile to arm64
   ];
+
+  LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
 
   shellHook = ''
     rustup toolchain install stable
+    cargo install cross --git https://github.com/cross-rs/cross # to cross compile to arm64
+    export PATH=$PATH:~/.cargo/bin/ # add cargo applications (cross, cross-util...) to path
   '';
 }
